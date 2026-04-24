5ada4b7a64ff88df6afa6fc9f2e27fae19bcaa0a
Mamun123123123 <almalm90459@gmail.com>
2026-04-25 00:46:03 +0600

    successfully sign up and data store in mongoDB

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,14 +3,24 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.AUTH_DB_URI);
-const db = client.db();
+
+let db;
+
+async function connectDB() {
+  if (!db) {
+    await client.connect(); // ✅ THIS LINE FIXES YOUR ERROR
+    db = client.db();
+    console.log("MongoDB connected");
+  }
+  return db;
+}
 
 export const auth = betterAuth({
-     emailAndPassword: { 
-    enabled: true, 
+  emailAndPassword: {
+    enabled: true,
   },
-  database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
-    client
+
+  database: mongodbAdapter(await connectDB(), {
+    client,
   }),
 });
